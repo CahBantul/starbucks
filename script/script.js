ca7b153ca194a5ac7ed44ed09ec75e061259d879
Cah Bantul <fardan.nozami@gmail.com>
2021-05-30 01:26:42 +0700

    commit pertama

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function imgSlider(e) {
+  document.querySelector(".starbucks").src = e;
+}
+
+function changeCircleColor(e) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = e;
+}
